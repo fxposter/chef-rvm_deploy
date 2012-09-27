15fa448701a46193ca35ebffb821fbe88b24041a
Pavel Forkert <fxposter@gmail.com>
2012-09-27 18:43:04 +0300

    release v0.0.8

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "pavelf@wix.com"
 license          "All rights reserved"
 description      "Installs/Configures rvm_deploy"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.7"
+version          "0.0.8"
 
 depends "rvm"
